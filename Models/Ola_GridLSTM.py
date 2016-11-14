1d4c55c238a64480fa6f097cc1b3fa7f8f3953a0
simejo <simen.johnsrud92@gmail.com>
2016-11-14 15:24:51 +0100

    fixed bug

--- Models/Ola_GridLSTM.py
+++ Models/Ola_GridLSTM.py
@@ -91,7 +91,6 @@ EOT_ID = 3
 UNK_ID = 4
 
 
-
 def read_data(source_path, target_path, max_size=None):
     """Read data from source and target files and put into buckets.
 
@@ -119,14 +118,14 @@ def read_data(source_path, target_path, max_size=None):
                 if counter % 100000 == 0:
                     print("  reading data line %d" % counter)
                     sys.stdout.flush()
-            source_ids = [int(x) for x in source.split()]
-            target_ids = [int(x) for x in target.split()]
-            #target_ids.append(EOS_ID)
-            for bucket_id, (source_size, target_size) in enumerate(_buckets):
-              if len(source_ids) < source_size and len(target_ids) < target_size:
-                data_set[bucket_id].append([source_ids, target_ids])
-                break
-            source, target = source_file.readline(), target_file.readline()
+                source_ids = [int(x) for x in source.split()]
+                target_ids = [int(x) for x in target.split()]
+                #target_ids.append(EOS_ID)
+                for bucket_id, (source_size, target_size) in enumerate(_buckets):
+                    if len(source_ids) < source_size and len(target_ids) < target_size:
+                        data_set[bucket_id].append([source_ids, target_ids])
+                        break
+                source, target = source_file.readline(), target_file.readline()
     return data_set
 
 
@@ -223,7 +222,6 @@ def train():
                 perplexity_summary = tf.Summary()
                 # Run evals on development set and print their perplexity.
                 for bucket_id in xrange(len(_buckets)):
-                    print(dev_set)
                     if len(dev_set[bucket_id]) == 0:
                         print("  eval: empty bucket %d" % (bucket_id))
                         continue
