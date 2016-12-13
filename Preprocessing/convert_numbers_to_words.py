0a14a631bc25f6c8e3772fa28899d8de84404b7f
simejo <simen.johnsrud92@gmail.com>
2016-12-13 15:18:02 +0100

    Added method to print the sentence

--- Preprocessing/convert_numbers_to_words.py
+++ Preprocessing/convert_numbers_to_words.py
@@ -1,6 +1,7 @@
 from create_vocabulary import read_vocabulary_from_file
 import tensorflow as tf
 
+
 def read_file_and_convert(file_path, vocab_path, num_lines=5):
     _, rev_vocab = read_vocabulary_from_file(vocab_path)
     with open(file_path) as file_object:
@@ -9,9 +10,20 @@ def read_file_and_convert(file_path, vocab_path, num_lines=5):
                 break
             num_lines -= 1
             print(line)
-            output = [tf.compat.as_str(rev_vocab[output]) for output in line.split(" ")]
-            print(output)
+            output = [tf.compat.as_str(rev_vocab[int(output)]) for output in line.split()]
+            print(get_printable_sentence(output))
 
 
-read_file_and_convert("./Example-Data/x_test.txt", "./Example-Data/vocabulary.txt")
+def get_printable_sentence(input_list):
+    sentence = ""
+    for word in input_list:
+        if word == '_EOS':
+            word = "."
+        sentence += word + " "
+
+    # Remove redundant space in the end
+    return sentence[:-1]
+
+
+read_file_and_convert("./x_test.txt", "./vocabulary.txt")
 
