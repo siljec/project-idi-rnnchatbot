0dd172cb4a501b9037e971e2a2f841e0a98cd6c0
simejo <simen.johnsrud92@gmail.com>
2017-03-29 12:37:21 +0200

    replaced word was not putted back into the sentence

--- Preprocessing/preprocessing_stateful.py
+++ Preprocessing/preprocessing_stateful.py
@@ -7,7 +7,7 @@ sys.path.insert(0, '../')
 from variables import folders, _buckets, tokens, paths_from_preprocessing_stateful as paths
 
 # This script will only work for the stateful preprocessing, as we will use several files for training
-
+folders = ['30']
 # Step 1: Filter out conversations that fits the bucket size (init: (30,30)), then save the file to the stateful folder
 
 def read_every_source_file_and_save_to_dest(dest_path):
@@ -64,8 +64,12 @@ def preprocess_on_stateful(path, bucket_size, file_name_number, misspellings_dic
             else:
                 current_user = user
             # Replace unknown words
+            sentence = ""
             for word in line_words:
-                replace_word_helper(word, fasttext_dictionary)
+                word = replace_word_helper(word, fasttext_dictionary)
+                sentence += word + " "
+            text = sentence[:-1]
+
 
             if user1_first_line:
                 init_user, previous_user = current_user, current_user
